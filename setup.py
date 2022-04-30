37158401630d73f1185d5284e2537c71d13bed59
Stephen Beechen <stephen@beechens.com>
2022-04-29 20:57:46 -0600

    Add missing stuff to setup.py

--- setup.py
+++ setup.py
@@ -6,11 +6,12 @@ with open("README.md", "r", encoding="utf-8") as fh:
 setup(
     name='rpi-intercom',
     packages=find_packages(include=['rpi-intercom']),
+    ackage_data={'rpi-intercom': ['data/rpi-intercom.service']},
     version='0.0.2',
     description='Intercom library for a raspberrypi',
     long_description=long_description,
     long_description_content_type="text/markdown",
-    install_requires=["pymumble", "pyaudio", "gpiozero", "schema", "pyyaml", "pyOpenSSL"],
+    install_requires=["pymumble", "pyaudio", "gpiozero", "schema", "pyyaml", "pyOpenSSL", "numpy"],
     author="Stephen Beechen",
     author_email="stephen@beechens.com",
     python_requires=">=3.9",
